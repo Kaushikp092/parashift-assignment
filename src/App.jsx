b053f888a4fa0601670e2df477e067ddfaacf6fc
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-27 16:07:52 +0530

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar/Navbar"
+
 function App() {
 
   return (
     <>
+    <Navbar />
       <h1>Parashift blog assignment</h1>
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+	return (
+		<>
+			<nav className="flex justify-between items-center px-6 py-6 shadow-md sticky top-0 bg-gray-500">
+                {/* logo */}
+				<img src="/logo.svg" alt="logo" className="h-6 w-auto" />
+
+                {/* Desktop blog */}
+				<div className="flex items-center space-x-3">
+					<span className="hidden md:block text-xl font-semibold text-red-900">Blog</span>
+					<button
+						className="md:hidden text-3xl"
+						onClick={() => setMenuOpen(!menuOpen)}
+					>
+						{menuOpen ? <FaTimes /> : <FaBars />}
+					</button>
+				</div>
+			</nav>
+
+            {/* Mobile view menu */}
+            {menuOpen && (
+                <div className="md:hidden bg-gray-400 p-4 text-center">
+                    <span className="text-xl font-semibold text-red-900">Blog</span>
+                </div>
+            )}
+		</>
+	);
+};
+
+export default Navbar;
