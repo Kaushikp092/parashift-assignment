c26971bfb261197cf7dede542ec8722077cc50f1
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-28 14:50:48 +0530

    added all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes,Route } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/home";
 import BlogListingPage from "./components/Blog/BlogListingPage";
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<BlogListingPage />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/blogs" element={<BlogListingPage />} />
+				<Route path="*" element={<h1>404 Page not Found</h1> } />
+			</Routes>
 		</>
 	);
 }
